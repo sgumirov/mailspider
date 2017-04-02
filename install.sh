db17e04453e87a4b015d1d0add4fdbe0cabae445
Shamil Gumirov <shamil@gumirov.com>
2017-04-02 23:23:50 +0700

    install script added

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo mkdir /usr/share/MailSpider
+sudo chown mailspider:mailspider /usr/share/MailSpider
+sudo cp MailSpider-1.0-SNAPSHOT-configs.jar /usr/share/MailSpider/
+sudo cp target/MailSpider-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/share/MailSpider/
