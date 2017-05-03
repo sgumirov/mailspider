85bc1948f56210f247b79524130239740dc570d0
Shamil Gumirov <shamil@gumirov.com>
2017-05-03 16:31:25 +0700

    runuser fix upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,25 +1,15 @@
-set -e
-git pull
-mvn clean compile assembly:single
-cp target/MailSpider-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/share/MailSpider/
-./jarconfig.sh
-cp MailSpider-1.0-SNAPSHOT-configs.jar /usr/share/MailSpider/
-systemctl restart mailspider
-systemctl status mailspider
-
-
 #!/bin/bash
 
 set -e
 FILE=target/MailSpider-1.3-SNAPSHOT-jar-with-dependencies.jar
 
-git pull
-mvn clean compile assembly:single
+runuser -u mailspider git pull
+runuser -u mailspider mvn clean compile assembly:single
 
 if [ -f $FILE ]; then
-  ./jarconfig.sh
-  cp $FILE /usr/share/MailSpider/MailSpider-jar-with-dependencies.jar
-  cp MailSpider-configs.jar /usr/share/MailSpider/
+  runuser -u mailspider ./jarconfig.sh
+  runuser -u mailspider cp $FILE /usr/share/MailSpider/MailSpider-jar-with-dependencies.jar
+  runuser -u mailspider cp MailSpider-configs.jar /usr/share/MailSpider/
   echo "Restarting service"
   systemctl restart mailspider
   systemctl status mailspider
