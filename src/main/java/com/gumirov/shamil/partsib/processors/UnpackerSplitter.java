c8d3d65e04f16da7cd1fd56997c967b3313c5c96
Shamil Gumirov <shamil@gumirov.com>
2017-05-08 20:02:35 +0700

    fixed 7zipjbinding size limit

--- src/main/java/com/gumirov/shamil/partsib/processors/UnpackerSplitter.java
+++ src/main/java/com/gumirov/shamil/partsib/processors/UnpackerSplitter.java
@@ -44,7 +44,7 @@ public class UnpackerSplitter {
     try {
       SevenZip.initSevenZipFromPlatformJAR();
       inArchive = SevenZip.openInArchive(null, // Choose format automatically
-          new ByteArrayStream(b, false, 102400));
+          new ByteArrayStream(b, false, Integer.MAX_VALUE)); //disable max size as per javadoc
       ISimpleInArchive simpleInArchive = inArchive.getSimpleInterface();
       for (ISimpleInArchiveItem item : simpleInArchive.getArchiveItems()) {
         if (!item.isFolder()) {
