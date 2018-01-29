8a9c1a3a74f9afaf24c5c40abeb7814c995049cf
Shamil Gumirov <shamilg1@gumirov.com>
2018-01-29 14:18:04 +0300

    version updated

--- src/main/java/com/gumirov/shamil/partsib/MainRouteBuilder.java
+++ src/main/java/com/gumirov/shamil/partsib/MainRouteBuilder.java
@@ -53,7 +53,7 @@ public class MainRouteBuilder extends RouteBuilder {
   public static final String PRICEHOOK_RULE = "pricehook.rule";
   public static final String CHARSET = "UTF-8";
   public static final String PRICEHOOK_TAGGING_RULES_HEADER = "com.gumirov.shamil.partsib.PRICEHOOK_TAGGING_HEADER";
-  public static final String VERSION = "1.3";
+  public static final String VERSION = "1.7";
   public static final String PLUGINS_STATUS_OK = "MAILSPIDER_PLUGINS_STATUS";
   public static final String SOURCE_ID = "server.source";
   /**
