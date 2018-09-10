d0d2368bc5521760b342054ee5f47dfa1dd6de4c
Shamil Gumirov <shamilg1@gmail.com>
2018-09-10 09:52:28 +0200

    Added additional safety layer to delete old mail route when parsing can go bad

--- src/main/java/com/gumirov/shamil/partsib/processors/DeleteOldMailProcessor.java
+++ src/main/java/com/gumirov/shamil/partsib/processors/DeleteOldMailProcessor.java
@@ -3,13 +3,16 @@ package com.gumirov.shamil.partsib.processors;
 import com.gumirov.shamil.partsib.MainRouteBuilder;
 import com.gumirov.shamil.partsib.util.SkipMessageException;
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import static com.gumirov.shamil.partsib.MainRouteBuilder.DAY_MILLIS;
+import static com.gumirov.shamil.partsib.MainRouteBuilder.MID;
 
 public class DeleteOldMailProcessor implements Processor {
   Logger log = LoggerFactory.getLogger(this.getClass());
@@ -31,7 +34,13 @@ public class DeleteOldMailProcessor implements Processor {
     if (dateHeader == null) {
       throw new SkipMessageException("No Date header for mail. Skipping: subj="+subj);
     }
-    Date mailDate = MainRouteBuilder.mailDateFormat.parse((String)dateHeader);
+    Date mailDate = null;
+    try {
+      mailDate = MainRouteBuilder.mailDateFormat.parse(dateHeader.toString());
+    } catch (ParseException e) {
+      log.warn("["+exchange.getIn().getHeader(MID)+"] Skipping message in delete_old_mail route as cannot parse message header Date: "+dateHeader);
+      throw new SkipMessageException("Skipping as cannot parse Date header: subj="+subj);
+    }
     if (mailDate.before(oldestStore)) {
       log.info(String.format("Delete old: date=%s, subj=%s, from=%s", date, subj, from));
       passedBy++;
